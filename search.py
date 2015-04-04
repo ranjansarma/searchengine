ef57b5397d8e6c8c84bfd755b9fc6fea01050275
Ranjan Sarmah <ranjan.sarma23@gmail.com>
2015-04-04 17:16:54 +0530

    removed unnecessary codes

--- search.py
+++ search.py
@@ -10,16 +10,9 @@ def main():
     ix = open_dir(index_dir)
     with ix.searcher() as searcher:
         query = QueryParser("content", ix.schema).parse(unicode(q))
-        corrected = searcher.correct_query(query,q)
-        if corrected.query != query:
-            print("Did you mean:", corrected.string)
-
         results = searcher.search(query, limit=None)
         for res in results:
             print 'title : ',  res['title']
-
-def escape_aps(text):
-    return text.replace("'", "&#39;")
 
 
 if __name__ == '__main__':
